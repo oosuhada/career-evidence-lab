e38d2a9e787928358f93b13efb99381adeb90a3d
oosuhada <185910926+oosuhada@users.noreply.github.com>
2025-09-10 09:37:22 +0900

    feat(ui): add icon system

---
+++ src/components/icons/ChevronArrowRightIcon.tsx
@@ -0,0 +1,13 @@
+import { type ComponentProps } from 'react';
+
+import Svg from '../svg/Svg';
+
+const ChevronArrowRightIcon = ({ width = 8, height = 14, color = '#C9CFDF', ...rest }: ComponentProps<typeof Svg>) => {
+  return (
+    <Svg width={width} height={height} {...rest}>
+      <path d="M1 13L7 7L1 1" stroke={color} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+    </Svg>
+  );
+};
+
+export default ChevronArrowRightIcon;
---
+++ src/components/icons/Icon.stories.tsx
@@ -0,0 +1,70 @@
+import { type Meta } from '@storybook/react';
+
+import ArrowIcon from './ArrowIcon';
+import BookmarkIcon from './BookmarkIcon';
+import BottomSheetHandleIcon from './BottomSheetHandleIcon';
+import CheckIcon from './CheckIcon';
+import ChevronArrowRightIcon from './ChevronArrowRightIcon';
+import EditIcon from './EditIcon';
+import LineThreeDotsIcon from './LineThreeDotsIcon';
+import SendIcon from './SendIcon';
+import ThreeDotsIcon from './ThreeDotsIcon';
+import WarningIcon from './WarningIcon';
+import XIcon from './XIcon';
+
+const meta: Meta = {
+  title: 'icon',
+};
+
+export default meta;
+
+export function Arrow() {
+  return (
+    <div>
+      <ArrowIcon />
+      <ArrowIcon direction="up" />
+      <ArrowIcon direction="right" />
+      <ArrowIcon direction="down" />
+    </div>
+  );
+}
+
+export function X() {
+  return <XIcon />;
+}
+
+export function LineThreeDots() {
+  return <LineThreeDotsIcon />;
+}
+
+export function BottomSheetHandle() {
+  return <BottomSheetHandleIcon />;
+}
+
+export function Send() {
+  return <SendIcon />;
+}
+
+export function ThreeDots() {
+  return <ThreeDotsIcon />;
+}
+
+export function Warning() {
+  return <WarningIcon />;
+}
+
+export function Edit() {
+  return <EditIcon />;
+}
+
+export function ChevronArrowRight() {
+  return <ChevronArrowRightIcon />;
+}
+
+export function Check() {
+  return <CheckIcon />;
+}
+
+export function Bookmark() {
+  return <BookmarkIcon isBookmarked={true} />;
+}
