ca149a3ce4dd3a31855eaf5c37ecaed77ce014c1
oosuhada <185910926+oosuhada@users.noreply.github.com>
2025-09-18 11:23:56 +0900

    feat: add input and navigation hooks

---
+++ src/hooks/router/useInternalRouter.ts
@@ -0,0 +1,32 @@
+import { type Url } from 'next/dist/shared/lib/router/router';
+import { useRouter } from 'next/router';
+
+type SurveyPaths = '/survey' | '/survey/intro' | '/survey/create' | '/survey/join' | '/survey/link';
+type ResultPaths = '/result';
+type FeedbackPaths = '/feedback' | `/feedback/${string}`;
+type GalleryPaths = '/gallery' | '/gallery/bookmarks';
+
+export type InternalPaths = '/' | SurveyPaths | ResultPaths | FeedbackPaths | GalleryPaths;
+
+interface TransitionOptions {
+  shallow?: boolean;
+  locale?: string | false;
+  scroll?: boolean;
+  unstable_skipClientCache?: boolean;
+}
+
+/**
+ * @description
+ * next useRouter hook의 push 메서드를 type safe하게 사용할 수 있는 훅 입니다.
+ * `push` 외의 메서드는 useRouter와 동일합니다.
+ */
+const useInternalRouter = () => {
+  const router = useRouter();
+
+  return {
+    ...router,
+    push: (path: InternalPaths, as?: Url, options?: TransitionOptions) => router.push(path, as, options),
+  };
+};
+
+export default useInternalRouter;
