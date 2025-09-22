88d1bd1c38435ad3d68af14237afa5e9f774532a
oosuhada <185910926+oosuhada@users.noreply.github.com>
2025-09-22 09:13:22 +0900

    feat: add business logic utilities

---
+++ src/utils/agent.ts
@@ -0,0 +1,71 @@
+const getNavigatorUserAgent = () => {
+  return typeof navigator === 'undefined' ? 'SSR' : navigator.userAgent;
+};
+
+export const isAndroid = () => {
+  const userAgent = getNavigatorUserAgent();
+
+  return Boolean(userAgent.match(/Android/i));
+};
+
+export const isIos = () => {
+  const userAgent = getNavigatorUserAgent();
+
+  return Boolean(userAgent.match(/iPhone|iPad|iPod/i));
+};
+
+export const isMobile = () => {
+  return Boolean(isAndroid() || isIos());
+};
+
+export const isSSR = () => {
+  const userAgent = getNavigatorUserAgent();
+
+  return Boolean(userAgent.match(/SSR/i));
+};
+
+export const isDesktop = () => {
+  return Boolean(!isMobile() && !isSSR());
+};
+
+const browsers = [
+  'Instagram',
+  'Chrome',
+  'Opera',
+  'WebTV',
+  'Whale',
+  'Beonex',
+  'Chimera',
+  'NetPositive',
+  'Phoenix',
+  'Firefox',
+  'Safari',
+  'SkipStone',
+  'Netscape',
+  'Mozilla',
+];
+
+/**
+ * @description 브라우저 종류를 알아내는 함수입니다.
+ * @returns string - 브라우저 이름 or 'Other'
+ */
+
+export const getBrowser = () => {
+  if (!window) return null;
+
+  const userAgent = window.navigator.userAgent.toLowerCase();
+
+  if (userAgent.includes('edg')) {
+    return 'Edge';
+  }
+
+  if (userAgent.includes('trident') || userAgent.includes('msie')) {
+    return 'Internet Explorer';
+  }
+
+  if (userAgent.includes('crios')) {
+    return 'Chrome';
+  }
+
+  return browsers.find((browser) => userAgent.includes(browser.toLowerCase())) || 'Other';
+};
